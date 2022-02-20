16a649de4ba3893ffef47129bfdd953e6e65fd48
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-02-20 15:55:53 +0900

    音量を制御できるよう修正

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,33 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { defineProps } from "vue";
+import { dispatch } from "../service/noise/dispatch";
 
 defineProps<{ msg: string }>();
 
-const count = ref(0);
+const vol = ref(0.2);
+
+const onPlayClick = () => {
+  dispatch({ type: "ChangeVolume", volume: vol.value });
+  dispatch({ type: "Play" });
+};
+const onStopClick = () => dispatch({ type: "Stop" });
+const onVolChange = () => dispatch({ type: "ChangeVolume", volume: vol.value });
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    See
-    <code>README.md</code> for more information.
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank"
-      >Vite Docs</a
-    >
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <button @click="onPlayClick">Play</button>
+  <button @click="onStopClick">Stop</button>
+  <input
+    v-model="vol"
+    type="range"
+    max="1.0"
+    min="0.0"
+    step="0.01"
+    @input="onVolChange"
+  />
 </template>
 
 <style scoped>
--- src/service/noise/context.ts
+++ src/service/noise/context.ts
@@ -8,15 +8,14 @@ for (let i = 0; i < bufferSize; i++) {
   output[i] = Math.random() * 2 - 1;
 }
 
-const gainNode = context.createGain();
-gainNode.gain.value = 0.0;
-
 const whiteNoise = context.createBufferSource();
+const gainNode = context.createGain();
+
+gainNode.connect(context.destination);
+whiteNoise.connect(gainNode);
 whiteNoise.buffer = noiseBuffer;
 whiteNoise.loop = true;
 whiteNoise.start(0);
-
-whiteNoise.connect(context.destination);
 
 export default {
   context,
